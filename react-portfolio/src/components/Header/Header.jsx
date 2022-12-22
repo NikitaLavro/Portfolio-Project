6d8e6c6d0b7625d7e97013e36ccba88b7e53ed2a
Nikita <lavro.nikita123@gmail.com>
2022-12-22 17:49:03 -0500

    Header styles

--- react-portfolio/src/components/Header/Header.jsx
+++ react-portfolio/src/components/Header/Header.jsx
@@ -11,28 +11,28 @@ const Header = () => {
         <div className="nav-menu">
           <ul className="nav__list grid">
             <li className="nav__item">
-              <a href="#home" className="nav__link">
-                <i className="uil uil-estate nav__icon">Home</i>
+              <a href="#home" className="nav__link active-link">
+                <i className="uil uil-estate nav__icon"></i>Home
               </a>
             </li>
             <li className="nav__item">
               <a href="#about" className="nav__link">
-                <i className="uil uil-user nav__icon">About</i>
+                <i className="uil uil-user nav__icon"></i>About
               </a>
             </li>
             <li className="nav__item">
               <a href="#skills" className="nav__link">
-                <i className="uil uil-file-alt nav__icon">Skills</i>
+                <i className="uil uil-file-alt nav__icon"></i>Skills
               </a>
             </li>
             <li className="nav__item">
               <a href="portfolio" className="nav__link">
-                <i className="uil uil-scenery nav__icon">Portfolio</i>
+                <i className="uil uil-scenery nav__icon"></i>Portfolio
               </a>
             </li>
             <li className="nav__item">
               <a href="contact" className="nav__link">
-                <i className="uil uil-message nav__icon">Contact</i>
+                <i className="uil uil-message nav__icon"></i>Contact
               </a>
             </li>
           </ul>
