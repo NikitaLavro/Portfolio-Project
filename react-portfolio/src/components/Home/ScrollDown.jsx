d70552172f01596b6cd7432a19eb5474ae89487b
Nikita <lavro.nikita123@gmail.com>
2022-12-23 18:34:32 -0500

    Home Scroll animation

--- react-portfolio/src/components/Home/ScrollDown.jsx
+++ react-portfolio/src/components/Home/ScrollDown.jsx
@@ -8,6 +8,8 @@ const ScrollDown = () => {
     <div className="home__scroll">
       <a href="#about" className="home__scroll-button button--flex">
         <Scroll />
+        <span className="home__scroll-name">Scroll Down</span>
+        <i className="uil uil-arrow-down home__scroll-arrow"></i>
       </a>
     </div>
   );
