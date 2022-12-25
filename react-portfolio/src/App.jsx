f207a7d61999e71349a64e1e63ce3b705f579f2a
Nikita <lavro.nikita123@gmail.com>
2022-12-25 18:38:06 -0500

    ScrollUp component and Css

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -6,6 +6,7 @@ import Home from "./components/Home/Home";
 import Skills from "./components/Skills/Skills";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
+import ScrollUp from "./components/ScrollUp/ScrollUp";
 
 function App() {
   return (
@@ -15,8 +16,9 @@ function App() {
         <Home />
         <Skills />
         <Contact />
-        <Footer />
       </main>
+      <Footer />
+      <ScrollUp />
     </div>
   );
 }
---
+++ react-portfolio/src/components/ScrollUp/ScrollUp.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+//CSS
+import "./ScrollUp.css";
+
+const ScrollUp = () => {
+  window.addEventListener("scroll", () => {
+    const scrollup = document.querySelector(".scrollup");
+    if (window.scrollY >= 560) {
+      scrollup.classList.add("show-scroll");
+    } else {
+      scrollup.classList.remove("show-scroll");
+    }
+  });
+
+  return (
+    <a href="#" className="scrollup">
+      <i className="uil uil-arrow-up scrollup__icon"></i>
+    </a>
+  );
+};
+
+export default ScrollUp;
