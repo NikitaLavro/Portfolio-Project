6bb95e44fc19cbcd196dbbc091cfe653d651723e
Nikita <lavro.nikita123@gmail.com>
2022-12-25 17:08:45 -0500

    Contact component initial jsx

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Skills from "./components/Skills/Skills";
+import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <main className="main">
         <Home />
         <Skills />
+        <Contact />
       </main>
     </div>
   );
