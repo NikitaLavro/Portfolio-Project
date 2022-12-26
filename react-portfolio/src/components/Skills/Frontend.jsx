95dde271b2103259de7515da972242b24366e23b
Nikita <lavro.nikita123@gmail.com>
2022-12-26 15:42:29 -0500

    Skills component code refactoring

--- react-portfolio/src/components/Skills/Frontend.jsx
+++ react-portfolio/src/components/Skills/Frontend.jsx
@@ -1,6 +1,34 @@
 import React from "react";
 
+//CSS
 import "./Skills.css";
+
+const frontendSkills = [
+  {
+    name: "React",
+    description: "intermediate",
+  },
+  {
+    name: "React",
+    description: "intermediate",
+  },
+  {
+    name: "React",
+    description: "intermediate",
+  },
+  {
+    name: "React",
+    description: "intermediate",
+  },
+  {
+    name: "React",
+    description: "intermediate",
+  },
+  {
+    name: "React",
+    description: "intermediate",
+  },
+];
 
 const Frontend = () => {
   return (
@@ -8,52 +36,15 @@ const Frontend = () => {
       <h3 className="skills__title">Frontend Development</h3>
 
       <div className="skills__box">
-        <div className="skills__group">
+        {frontendSkills.map((skill) => (
           <div className="skills__data">
             <i className="bx bx-badge-check"></i>
             <div>
-              <h3 className="skills__name">React</h3>
-              <span className="skills__level">Intermediate</span>
+              <h3 className="skills__name">{skill.name}</h3>
+              <span className="skills__level">{skill.description}</span>
             </div>
           </div>
-          <div className="skills__data">
-            <i className="bx bx-badge-check"></i>
-            <div>
-              <h3 className="skills__name">React</h3>
-              <span className="skills__level">Intermediate</span>
-            </div>
-          </div>
-          <div className="skills__data">
-            <i className="bx bx-badge-check"></i>
-            <div>
-              <h3 className="skills__name">React</h3>
-              <span className="skills__level">Intermediate</span>
-            </div>
-          </div>
-        </div>
-        <div className="skills__group">
-          <div className="skills__data">
-            <i className="bx bx-badge-check"></i>
-            <div>
-              <h3 className="skills__name">React</h3>
-              <span className="skills__level">Intermediate</span>
-            </div>
-          </div>
-          <div className="skills__data">
-            <i className="bx bx-badge-check"></i>
-            <div>
-              <h3 className="skills__name">React</h3>
-              <span className="skills__level">Intermediate</span>
-            </div>
-          </div>
-          <div className="skills__data">
-            <i className="bx bx-badge-check"></i>
-            <div>
-              <h3 className="skills__name">React</h3>
-              <span className="skills__level">Intermediate</span>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
--- react-portfolio/src/components/Skills/General.jsx
+++ react-portfolio/src/components/Skills/General.jsx
@@ -3,58 +3,47 @@ import React from "react";
 //CSS
 import "./Skills.css";
 
+const generalSkills = [
+  {
+    name: "NPM",
+    description: "intermediate",
+  },
+  {
+    name: "NPM",
+    description: "intermediate",
+  },
+  {
+    name: "NPM",
+    description: "intermediate",
+  },
+  {
+    name: "NPM",
+    description: "intermediate",
+  },
+  {
+    name: "NPM",
+    description: "intermediate",
+  },
+  {
+    name: "NPM",
+    description: "intermediate",
+  },
+];
+
 const General = () => {
   return (
     <div className="skills__content">
       <h3 className="skills__title">General</h3>
-
       <div className="skills__box">
-        <div className="skills__group">
+        {generalSkills.map((skill) => (
           <div className="skills__data">
             <i className="bx bx-badge-check"></i>
             <div>
-              <h3 className="skills__name">NPM</h3>
-              <span className="skills__level">Itermediate</span>
+              <h3 className="skills__name">{skill.name}</h3>
+              <span className="skills__level">{skill.description}</span>
             </div>
           </div>
-          <div className="skills__data">
-            <i className="bx bx-badge-check"></i>
-            <div>
-              <h3 className="skills__name">NPM</h3>
-              <span className="skills__level">Itermediate</span>
-            </div>
-          </div>
-          <div className="skills__data">
-            <i className="bx bx-badge-check"></i>
-            <div>
-              <h3 className="skills__name">NPM</h3>
-              <span className="skills__level">Itermediate</span>
-            </div>
-          </div>
-        </div>
-        <div className="skills__group">
-          <div className="skills__data">
-            <i className="bx bx-badge-check"></i>
-            <div>
-              <h3 className="skills__name">NPM</h3>
-              <span className="skills__level">Itermediate</span>
-            </div>
-          </div>
-          <div className="skills__data">
-            <i className="bx bx-badge-check"></i>
-            <div>
-              <h3 className="skills__name">NPM</h3>
-              <span className="skills__level">Itermediate</span>
-            </div>
-          </div>
-          <div className="skills__data">
-            <i className="bx bx-badge-check"></i>
-            <div>
-              <h3 className="skills__name">NPM</h3>
-              <span className="skills__level">Itermediate</span>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
