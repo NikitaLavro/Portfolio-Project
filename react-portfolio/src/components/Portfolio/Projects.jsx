a6b7ebf57021b31982b4c42e8299f7f80b7a9e61
Nikita <lavro.nikita123@gmail.com>
2022-12-26 16:08:10 -0500

    Projects initial jsx

--- react-portfolio/src/components/Portfolio/Projects.jsx
+++ react-portfolio/src/components/Portfolio/Projects.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
+import WorkImage from "../../assets/portrait.jpg";
+
+const projectsData = [
+  {
+    id: 1,
+    image: WorkImage,
+    title: "test title",
+  },
+  {
+    id: 2,
+    image: WorkImage,
+    title: "test title",
+  },
+  {
+    id: 3,
+    image: WorkImage,
+    title: "test title",
+  },
+];
+
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div className="work__container container grid">
+      {projectsData.map((item) => (
+        <div key={item.id}>{item.title}</div>
+      ))}
+    </div>
+  );
 };
 
 export default Projects;
