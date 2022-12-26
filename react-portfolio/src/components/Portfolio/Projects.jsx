3ba937aea99a4410517d7fb531683363df062369
Nikita <lavro.nikita123@gmail.com>
2022-12-26 17:04:35 -0500

    Projects jsx and styles

--- react-portfolio/src/components/Portfolio/Projects.jsx
+++ react-portfolio/src/components/Portfolio/Projects.jsx
@@ -11,18 +11,31 @@ const projectsData = [
     id: 1,
     image: WorkImage,
     title: "test title",
+    technologies: ["react", "react", "react"],
+    description:
+      "asdsadasdasdasdasdasd dasdashdkjasd asjdjashdjashdasj jasdhajskhdjas jasdjkasdhaskjd ajsdhjkasdh",
   },
   {
     id: 2,
     image: WorkImage,
     title: "test title",
+    technologies: ["react", "react", "react"],
+    description:
+      "asdsadasdasdasdasdasd dasdashdkjasd asjdjashdjashdasj jasdhajskhdjas jasdjkasdhaskjd ajsdhjkasdh",
   },
   {
     id: 3,
     image: WorkImage,
     title: "test title",
+    technologies: ["react", "react", "react"],
+    description:
+      "asdsadasdasdasdasdasd dasdashdkjasd asjdjashdjashdasj jasdhajskhdjas jasdjkasdhaskjd ajsdhjkasdh",
   },
 ];
+
+function firstLetterUpperCase(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 const Projects = () => {
   return (
@@ -30,7 +43,20 @@ const Projects = () => {
       {projectsData.map((item) => (
         <div className="project__card" key={item.id}>
           <img src={item.image} alt={item.title} className="project__img" />
-          <h3 className="project__title">{item.title}</h3>
+          <h3 className="project__title">{firstLetterUpperCase(item.title)}</h3>
+          <div className="project__technologies-container">
+            <h5 className="project__subtitle">Technologies:</h5>
+            <p className="project__technologies">
+              {firstLetterUpperCase(item.technologies.join(" , "))}
+            </p>
+          </div>
+          <div className="project__description-container">
+            <h5 className="project__subtitle">Description:</h5>
+            <p className="project__description">
+              {firstLetterUpperCase(item.description)}
+            </p>
+          </div>
+          <div className="project__line-break" />
           <a href="" className="project__button">
             Demo <i className="bx bx-right-arrow-alt project__button-icon"></i>
           </a>
