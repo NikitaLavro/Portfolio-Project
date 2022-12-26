1145677a5924eec8e489d7ed9a123483f552414e
Nikita <lavro.nikita123@gmail.com>
2022-12-26 18:15:21 -0500

    Project Swiper functionality

--- react-portfolio/src/components/Portfolio/Projects.jsx
+++ react-portfolio/src/components/Portfolio/Projects.jsx
@@ -1,4 +1,14 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper";
 
 //CSS
 import "./Portfolio.css";
@@ -8,6 +18,7 @@ import WorkImage from "../../assets/portrait.jpg";
 
 //Components
 import ProjectCard from "./ProjectCard";
+import { Scrollbar } from "swiper";
 
 const projectsData = [
   {
@@ -37,14 +48,59 @@ const projectsData = [
       "asdsadasdasdasdasdasd dasdashdkjasd asjdjashdjashdasj jasdhajskhdjas jasdjkasdhaskjd ajsdhjkasdh",
     link: "https://github.com/NikitaLavro",
   },
+  {
+    id: 4,
+    image: WorkImage,
+    title: "test title",
+    technologies: ["react", "react", "react"],
+    description:
+      "asdsadasdasdasdasdasd dasdashdkjasd asjdjashdjashdasj jasdhajskhdjas jasdjkasdhaskjd ajsdhjkasdh",
+    link: "https://github.com/NikitaLavro",
+  },
+  {
+    id: 5,
+    image: WorkImage,
+    title: "test title",
+    technologies: ["react", "react", "react"],
+    description:
+      "asdsadasdasdasdasdasd dasdashdkjasd asjdjashdjashdasj jasdhajskhdjas jasdjkasdhaskjd ajsdhjkasdh",
+    link: "https://github.com/NikitaLavro",
+  },
 ];
 
 const Projects = () => {
   return (
     <div className="project__container container">
-      {projectsData.map((item) => (
-        <ProjectCard key={item.id} item={item} />
-      ))}
+      <Swiper
+        grabCursor={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+        breakpoints={{
+          0: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+
+          576: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+
+          900: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+          },
+        }}
+      >
+        {projectsData.map((item) => (
+          <SwiperSlide>
+            <ProjectCard key={item.id} item={item} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
